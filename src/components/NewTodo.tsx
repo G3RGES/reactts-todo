ebc39b403f7e9d08e27100a7b96c0bd4ababc0f7
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-27 14:04:45 +0200

    Update NewTodo.tsx

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewTodo = () => {
+  const todoTextRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    // const enteredText = todoTextRef.current?.value;
+
+    //* ONLY USE THIS IF I'M SURE THE ref WILL NEVER BE NULL
+    const enteredText = todoTextRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextRef} />
       <button>Add Todo</button>
     </form>
   );
