dcc553a82f06ba90f3554d72070bf2c78c9348c3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-28 14:43:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
+// import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import TodosContextProvider from "./store/todos-context";
+// import Todo from "./models/todo";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [todos, setTodos] = useState<Todo[]>([]);
 
-  // const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  // // const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
 
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
+  // const addTodoHandler = (todoText: string) => {
+  //   const newTodo = new Todo(todoText);
 
-    setTodos((prevTodo) => {
-      return prevTodo.concat(newTodo);
-    });
-  };
+  //   setTodos((prevTodo) => {
+  //     return prevTodo.concat(newTodo);
+  //   });
+  // };
 
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
-    });
-  };
+  // const removeTodoHandler = (todoId: string) => {
+  //   setTodos((prevTodos) => {
+  //     return prevTodos.filter((todo) => todo.id !== todoId);
+  //   });
+  // };
 
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} removeTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
