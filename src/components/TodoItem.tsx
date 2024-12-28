28feac070cafc065e561217cad1850042331ebaa
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-28 13:04:35 +0200

    Update TodoItem.tsx

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<{
+  text: string;
+  removeTodo: () => void;
+}> = (props) => {
+  return (
+    <li className={classes.item} onClick={props.removeTodo}>
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
